a437d69766159dffa924f6c2928bfbebf9fa8adc
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-05-01 18:04:50 -0700

    finished test for users's migrations, routes, and seeds

--- migrations/2_exercises.js
+++ migrations/2_exercises.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('exercises', (table) => {
     table.increments();
-    table.integer('user_id').reference('id').inTable('users').onDelete('CASCADE').notNullable().index();
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE').notNullable().index();
     table.string('notes_array').notNullable().defaultTo('[]');
     table.timestamps(true, true);
   });
--- migrations/3_scores.js
+++ migrations/3_scores.js
@@ -1,8 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable('scores', (table) => {
     table.increments();
-    table.integer('exercises_id').reference('id').inTable('exercise').onDelete('CASCADE').notNullable().index();
-    table.string('scores_array').notNullable.defaultTo('[]');
+    table.integer('exercises_id').references('id').inTable('exercises').onDelete('CASCADE').notNullable().index();
+    table.string('scores_array').notNullable().defaultTo('[]');
     table.float('avg_score').notNullable().defaultTo(0);
     table.timestamps(true, true);
   });
