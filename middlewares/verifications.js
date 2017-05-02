40bd6b1d90fdd77571ef891ed26a9fe2c5dee035
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-05-02 10:48:19 -0700

    old thing

--- middlewares/verifications.js
+++ middlewares/verifications.js
@@ -2,9 +2,10 @@ const jwt = require('jsonwebtoken');
 // console.log('did i use middleware?');
 
 function middlewareVerify(req, res, next){
+  // console.log("what is cookie?", req.headers);
   jwt.verify(req.headers.cookie.substring(5), process.env.JWT_KEY, (err, payload) => {
     if (err) {
-      console.log('did i make an error?');
+      // console.log('did i make an error?');
       res.status(401);
       res.send({ status: 401, ErrorMessage: 'Unauthorized' });
     }
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,27 @@
 const bcrypt = require('bcrypt-as-promised');
 const boom = require('boom');
 const express = require('express');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const bodyParser = require('body-parser');
 const { camelizeKeys, decamelizeKeys } = require('humps');
 const  { middlewareVerify } = require('../middlewares/verifications.js');
 const router = express.Router();
 
-// router.get('/users', middlewareVerify );
+router.get('/users', function middlewareVerify(req, res, next){
+  console.log("what is cookie?", req.headers);
+  jwt.verify(req.headers.cookie.substring(5), process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      console.log('did i make an error?');
+      res.status(401);
+      res.send({ status: 401, ErrorMessage: 'Unauthorized' });
+    }
+    else{
+      tokenId = payload.userId;
+      next();
+    }
+  });
+});
 
 router.get('/users', (req, res, next) => {
   return knex('users').select('id', 'first_name', 'last_name', 'email').then((users) => {
