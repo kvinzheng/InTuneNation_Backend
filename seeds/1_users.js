6bce011712a15f0a5bb9641ae3b5189badfb1466
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-05-02 08:50:57 -0700

    updated user seed

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -38,7 +38,34 @@ exports.seed = function (knex, Promise) {
         hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
         created_at: new Date('2016-06-29 14:26:16 UTC'),
         updated_at: new Date('2016-06-29 14:26:16 UTC'),
-      }
+      },
+      {
+        id: 5,
+        first_name: 'Michael',
+        last_name: 'Martinez',
+        email: 'martinez1212@gmail.com',
+        hashed_password: '$2a$12$C9AGYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
+        created_at: new Date('2016-06-29 14:26:16 UTC'),
+        updated_at: new Date('2016-06-29 14:26:16 UTC'),
+      },
+      {
+        id: 6,
+        first_name: 'Ryan',
+        last_name: 'Thissen',
+        email: 'rmt1855@gmail.com',
+        hashed_password: '$2a$12$C9AGYmcLVGYlGoO4SSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
+        created_at: new Date('2016-06-29 14:26:16 UTC'),
+        updated_at: new Date('2016-06-29 14:26:16 UTC'),
+      },
+      {
+        id: 7,
+        first_name: 'Alexander',
+        last_name: 'KKrawiec',
+        email: 'alexanderkrawiec@gmail.com',
+        hashed_password: '$2a$12$C9AGYmcLVGYlGoO4SSZTPud6ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
+        created_at: new Date('2016-06-29 14:26:16 UTC'),
+        updated_at: new Date('2016-06-29 14:26:16 UTC'),
+      },
     ]);
   }).then(() => {
     return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));");
