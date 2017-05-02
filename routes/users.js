da1e070f402b33a0a33a6616be28f37b445b4b3d
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-05-02 00:59:15 -0700

    fixed bodyparser.json()

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt-as-promised');
 const boom = require('boom');
 const express = require('express');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const bodyParser = require('body-parser');
 const { camelizeKeys, decamelizeKeys } = require('humps');
