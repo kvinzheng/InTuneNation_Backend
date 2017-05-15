6f7471f9e97f5849600a0aa572c6630e6ff37eb2
Robert Murray <work@robertmurray.me>
2017-05-14 21:30:25 -0700

    made many suggestions of things to cleanup and comments. Removed node_modules from the repository. Removed .env from the repository.

--- server.js
+++ server.js
@@ -1,63 +1,73 @@
 // reading environmental variable
-if (process.envNODE_ENV !== 'production') {
-  require('dotenv').config();
+if ( process.envNODE_ENV !== 'production' ) {
+    require( 'dotenv' ).config();
 }
 
-const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
+const express = require( 'express' );
+const cors = require( 'cors' );
+const passport = require( 'passport' );
+const bodyParser = require( 'body-parser' );
+const cookieParser = require( 'cookie-parser' );
+const morgan = require( 'morgan' );
 const app = express();
 
-const { middlewareVerify } = require('./middlewares/verifications.js');
-const user = require('./routes/users');
-const configAuth = require('./config/auth');
-const exercises = require('./routes/exercises');
-const scores = require('./routes/scores');
-const noteScore = require('./routes/noteScore');
+const {
+    middlewareVerify
+} = require( './middlewares/verifications.js' );
+// where is your middleware being used?
+const user = require( './routes/users' );
+const configAuth = require( './config/auth' );
+const exercises = require( './routes/exercises' );
+const scores = require( './routes/scores' );
+const noteScore = require( './routes/noteScore' );
 
-app.disable('x-powered-by');
-switch (app.get('env')) {
-  case 'development':
-    app.use(morgan('dev'));
+app.disable( 'x-powered-by' );
+
+// the switch isn't the best option when you only have 2 options.
+// use and if / else here instead
+switch ( app.get( 'env' ) ) {
+case 'development':
+    app.use( morgan( 'dev' ) );
     break;
 
-  case 'production':
-    app.use(morgan('short'));
+case 'production':
+    app.use( morgan( 'short' ) );
     break;
 
-  default:
+default:
 }
-app.use(cors());
-app.options('*', cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use(passport.initialize());
+
+app.use( cors() );
+app.options( '*', cors() );
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( {
+    extended: true
+} ) );
+app.use( cookieParser() );
+app.use( passport.initialize() );
 
 
 // app.use('/users', middlewareVerify);
 //app.user(users) only apply for login & sign up & get all user
 //this doesn't apply the middlewares;
-app.use(user);
-app.use(exercises);
-app.use(scores);
-app.use(noteScore);
+app.use( user );
+app.use( exercises );
+app.use( scores );
+app.use( noteScore );
 
-app.use((_req, res) => {
-  res.sendStatus(404);
-});
+app.use( ( req, res ) => {
+    res.sendStatus( 404 );
+} );
 
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  if (app.get('env') !== 'test') {
-    console.log('Listening on port', port);
-  }
-});
+app.listen( port, () => {
+    if ( app.get( 'env' ) !== 'test' ) {
+        console.log( 'Listening on port', port );
+    }
+} );
 
 module.exports = app;
