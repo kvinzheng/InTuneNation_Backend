a97e083d7ce78059bdf2bf3479c2b8df69f3a53e
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-05-21 17:02:55 -0700

    updated validation

---
+++ validations/users.js
@@ -0,0 +1,18 @@
+
+const Joi = require('joi');
+
+ module.exports.post = {
+  body: {
+    email: Joi.string()
+      .label('Email')
+      .required()
+      .email()
+      .trim(),
+
+    password: Joi.string()
+      .label('Password')
+      .required()
+      .trim()
+      .min(8)
+  }
+};
