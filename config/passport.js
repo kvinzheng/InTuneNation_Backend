7f828e33ece3d257643cc37786b76ec03f275f8e
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-06-12 17:45:49 -0700

    refactored google OAuth

--- config/passport.js
+++ config/passport.js
@@ -1,38 +1,82 @@
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
-
-const configAuth = require('./auth.js');
+const passport = require('passport');
 require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-// const passportInfo = ( passport ) => {
-//   passport.use( new GoogleStrategy( {
-//     clientID: process.env.clientID,
-//     clientSecret: process.env.clientSecret,
-//     callbackURL: configAuth.googleAuth.callbackURL
-//   }, ( token, accessToken, refreshToken, profile, done ) => {
-//     process.nextTick( () => {
-//       console.log( profile );
-//       const user = {
-//         first_name: profile.name.givenName,
-//         last_name: profile.name.familyName,
-//         email: profile.emails[ 0 ].value
-//       };
-//       done( null, user );
-//     } );
-//   } ) );
-// }
+const knex = require('../knex');
+const session = require('express-session')
+const express = require('express');
+const RedisStore = require('connect-redis')(session);
+const app = express();
+const morgan = require('morgan');
+const router = express.Router();
 
+if (app.get('env') === 'development') {
+  app.use(morgan('dev'));
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+let newUser;
 passport.use(new GoogleStrategy({
-    clientID:    process.env.clientID,
-    clientSecret: process.env.clientSecret,
-    callbackURL: configAuth.googleAuth.callbackURL
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    // console.log('here?');
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.CALL_BACK_URL,
+  passReqToCallback: true
+}, function(request, accessToken, refreshToken, profile, done) {
+  newUser = {
+    first_name: profile.name.givenName,
+    last_name: profile.name.familyName,
+    email: profile.email,
+    hashed_password: 'null',
+    profile_picture: profile.photos[0].value
   }
-));
 
-module.exports = passportInfo;
+  knex('users').where('email', newUser.email).first().then((user) => {
+    if (user) {
+      //user exist in the user table
+      return done(null, newUser);
+      // return;
+    } else {
+      // user hasn't been in the user table yet
+      knex('users').insert((newUser), '*').catch((err) => console.log('Google did not authenticate you'));
+      return done(null, newUser);
+    }
+  });
+}));
+
+router.use(session({
+  secret: 'cookie_secret',
+  name: 'kaas',
+  store: new RedisStore({host: '127.0.0.1', port: 6379}),
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read']
+}));
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/auth/google/success',
+  failureRedirect: '/auth/google/failure'
+}));
+router.get('/auth/google/success', (req, res, next) => {
+  return res.json(newUser);
+});
+router.get('/auth/google/failure', (req, res, next) => {
+  return res.json('user is not authenticated yet');
+});
+
+module.exports = {
+  googleRouter: router
+};
--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+
 const app = express();
 const user = require('./routes/users');
 const configAuth = require('./config/auth');
 const exercises = require('./routes/exercises');
 const scores = require('./routes/scores');
 const noteScore = require('./routes/noteScore');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const {googleRouter} = require('./config/passport.js');
 const knex = require('./knex');
-const RedisStore = require('connect-redis')(session);
 
 app.disable('x-powered-by');
 
-if ( app.get('env') === 'development' ) {
-  app.use( morgan( 'dev' ) );
+if (app.get('env') === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(cors());
@@ -33,72 +33,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-let newUser;
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALL_BACK_URL,
-  passReqToCallback: true
-}, function(request, accessToken, refreshToken, profile, done) {
-  newUser = {
-    first_name: profile.name.givenName,
-    last_name: profile.name.familyName,
-    email: profile.email,
-    hashed_password: 'null',
-    profile_picture: profile.photos[0].value
-  }
-
-  knex('users').where('email', newUser.email).first().then((user) => {
-    if (user) {
-      //user exist in the user table
-      return done(null, newUser);
-      // return;
-    } else {
-      // user hasn't been in the user table yet
-      knex('users').insert((newUser), '*').catch((err) => console.log('Google did not authenticate you'));
-      return done(null, newUser);
-    }
-  });
-}));
-
-app.use(session({
-  secret: 'cookie_secret',
-  name: 'kaas',
-  store: new RedisStore({host: '127.0.0.1', port: 6379}),
-  proxy: true,
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: '/auth/google/success',
-  failureRedirect: '/auth/google/failure'
-}));
-app.get('/auth/google/success', (req, res, next) => {
-  return res.json(newUser);
-});
-app.get('/auth/google/failure', (req, res, next) => {
-  return res.json('user is not authenticated yet');
-});
-
 const {middlewareVerify} = require('./middlewares/verifications.js');
 app.use('/users', middlewareVerify);
 //app.user(users) only apply for login & sign up & get all user
+
 //this doesn't apply the middlewares;
+app.use(googleRouter); //applying google OAuth Routes
 app.use(user);
 app.use(exercises);
 app.use(scores);
