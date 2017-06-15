4e5640b1845388f415fdb35ddf72e70b1c07d023
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-06-14 22:35:49 -0700

    updated url

--- config/passport.js
+++ config/passport.js
@@ -92,9 +92,10 @@ router.get('/auth/google/success', (req, res, next) => {
   knex('users').where('email', newUser.email).first().then((user) => {
     result = user;
     console.log('result==',result);
+    let string = encodeURIComponent(JSON.stringify(result));
+    res.redirect('http://localhost:3000/profile/?' + string);
   });
-  let string = encodeURIComponent(JSON.stringify(result));
-  res.redirect('http://localhost:3000/?' + string);
+
 });
 router.get('/auth/google/failure', (req, res, next) => {
   return res.json('user is not authenticated yet');
