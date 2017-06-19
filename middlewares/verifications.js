f95ff7e084358de11261ae16a1fdec4233ce8cd2
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-06-19 11:12:29 -0700

    cleaned logs

--- middlewares/verifications.js
+++ middlewares/verifications.js
@@ -1,11 +1,8 @@
 const jwt = require('jsonwebtoken');
-// console.log('did i use middleware?');
 
 function middlewareVerify(req, res, next){
-  console.log('what is req.headers now kevin?', req.headers);
   jwt.verify(req.headers.token, process.env.JWT_KEY, (err, payload) => {
     if (err) {
-      // console.log('did i make an error?');
       res.status(401);
       res.send({ status: 401, ErrorMessage: 'Unauthorized' });
     }
